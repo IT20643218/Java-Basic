c60eba60588f61bff8e1375d6f62fd63e493ba88
sumedha lakmal bandaranayaka <74480291+sumedhaLakmal@users.noreply.github.com>
2021-06-26 02:42:09 +0530

    These are the first file for Java basic discussion

---
+++ java/ex1.java
@@ -0,0 +1,6 @@
+//Java program
+public class ex1{
+	public static void main(String args[]){
+		System.out.println("Hello World");
+	}//end of the main method
+}//end of the class
---
+++ java/ex2.java
@@ -0,0 +1,8 @@
+public class ex2{
+	public static void main(String args[]){
+		System.out.println("Sumedha Lakmal Bandaranayaka");
+		System.out.println("SLIIT");
+		System.out.println("Malabe");
+	}//end of the main function
+
+}//end of the class
