62f01d5f477e77196dec64f170dbcf6b21d6b86b
Sumedha <sumedhalakmal95@gmail.com>
2021-08-10 02:49:38 +0530

    Last Update 2021.08.10

---
+++ Java Lecture Code/FinalProper.java
@@ -0,0 +1,16 @@
+public class FinalProper{
+	private String Employeename;
+		
+	public FinalProper(String ename){
+		Employeename = ename;
+	}
+	
+	public String getEmpname()
+	{
+		return Employeename;
+	}
+	public void setName(String sname)
+	{
+		Employeename = sname;
+	}
+}
---
+++ Java Lecture Code/JavaApplication3.java
@@ -0,0 +1,12 @@
+public class JavaApplication3{
+	
+	public static void main (String[]args)
+	{
+	 FinalProper emp1 = new FinalProper("Kumara");
+	 FinalProper emp2 = new FinalProper("Saman");
+	 
+	 System.out.println(emp1.getEmpname());
+	 System.out.println(emp2.getEmpname());
+	}
+
+}
